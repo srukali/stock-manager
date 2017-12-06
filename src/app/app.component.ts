94f740457486a186126a861d43f16254a6811b1c
srukali <katelynicole.smith@gmail.com>
2017-12-05 21:59:54 -0800

    Updated datatable column names to reflect stock data, Added mock data for market overview and news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,122 +15,103 @@ export class AppComponent  {
   name = 'Stock Wizard';
 
   configWidthColumns: ITdDataTableColumn[] = [
-    { name: 'first_name',  label: 'First name', width: 150 },
-    { name: 'last_name', label: 'Last name', width: { min: 150, max: 250 }},
-    { name: 'gender', label: 'Gender'},
-    { name: 'email', label: 'Email', width: 250},
-    { name: 'img', label: '', width: 100},
+    { name: 'symbol',  label: 'Symbol' },
+    { name: 'last', label: 'Last' },
+    { name: 'change', label: 'Change' },
+    { name: 'percent_change', label: '% Change' },
+    { name: 'volume', label: 'Volume' },
   ];
 
   alerts = [
     {
-      symbol: "HD",
-      alert: "New 5 Day High of 26.500"
+      "symbol": "HD",
+      "alert": "New 5 Day High of 26.500"
     },
     {
-      symbol: "GSL",
-      alert: "New 5 Day Volume Breakout of 40,009"
+      "symbol": "GSL",
+      "alert": "New 5 Day Volume Breakout of 40,009"
     },
     {
-      symbol: "PNY",
-      alert: "New 5 Day Low of 12.450"
+      "symbol": "PNY",
+      "alert": "New 5 Day Low of 12.450"
     },
     {
-      symbol: "DFT",
-      alert: "New 5 Day High of 25.193"
+      "symbol": "DFT",
+      "alert": "New 5 Day High of 25.193"
     }
   ];
 
-  basicData = [
+  marketData = [
   {
-    "balance": 7454.6,
-    "email": "sclutterham0@123-reg.co.uk",
-    "first_name": "Sully",
-    "gender": "Male",
-    "img": "https://robohash.org/similiquemodiautem.bmp?size=50x50&set=set1",
-    "ip_address": "158.0.165.138",
-    "last_name": "Clutterham"
-  },
+    "symbol": "BAC",
+    "last": "29.06",
+    "change": "+0.96",
+    "percent_change": "+3.42%",
+    "volume": "141.88M"
+  }, {
+    "symbol": "XLF",
+    "last": "28.00",
+    "change": "+0.42",
+    "percent_change": "+3.52%",
+    "volume": "96.77M"
+  }, {
+    "symbol": "SPY",
+    "last": "264.14",
+    "change": "-0.32",
+    "percent_change": "-0.12%",
+    "volume": "83.44M"
+  }, {
+    "symbol": "EEM",
+    "last": "45.93",
+    "change": "+0.09",
+    "percent_change": "+0.20%",
+    "volume": "65.72M"
+  }, {
+    "symbol": "GE",
+    "last": "17.95",
+    "change": "+0.07",
+    "percent_change": "+0.39%",
+    "volume": "65.68M"
+  }, {
+    "symbol": "VXX",
+    "last": "32.59",
+    "change": "-0.24",
+    "percent_change": "-0.73%",
+    "volume": "53.98M"
+  }, {
+    "symbol": "CHX",
+    "last": "22.19",
+    "change": "-0.14",
+    "percent_change": "-2.73%",
+    "volume": "46.99M"
+  }
+];
+
+stories = [
   {
-    "balance": 3561.4,
-    "email": "mevason1@usatoday.com",
-    "first_name": "Mateo",
-    "gender": "Male",
-    "img": "https://robohash.org/molestiaeadquia.bmp?size=50x50&set=set1",
-    "ip_address": "68.147.207.137",
-    "last_name": "Evason"
-  },
-  {
-    "balance": 4456.3,
-    "email": "lgardener2@wordpress.org",
-    "first_name": "Lira",
-    "gender": "Female",
-    "img": "https://robohash.org/laboredolorumvelit.jpg?size=50x50&set=set1",
-    "ip_address": "96.85.6.31",
-    "last_name": "Gardener"
-  },
-  {
-    "balance": 5938,
-    "email": "edunckley3@instagram.com",
-    "first_name": "Edvard",
-    "gender": "Male",
-    "img": "https://robohash.org/ullamquaedeleniti.png?size=50x50&set=set1",
-    "ip_address": "233.189.117.211",
-    "last_name": "Dunckley"
-  },
-  {
-    "balance": 4241.6,
-    "email": "gsouza4@squidoo.com",
-    "first_name": "Gwynne",
-    "gender": "Female",
-    "img": "https://robohash.org/possimusrepellendusodio.png?size=50x50&set=set1",
-    "ip_address": "164.226.80.40",
-    "last_name": "Souza"
-  },
-  {
-    "balance": 6558,
-    "email": "sfurmedge5@furl.net",
-    "first_name": "Sena",
-    "gender": "Female",
-    "img": "https://robohash.org/iustoillumsit.png?size=50x50&set=set1",
-    "ip_address": "192.214.177.38",
-    "last_name": "Furmedge"
-  },
-  {
-    "balance": 3159.2,
-    "email": "cdykes6@china.com.cn",
-    "first_name": "Christian",
-    "gender": "Male",
-    "img": "https://robohash.org/exveniama.jpg?size=50x50&set=set1",
-    "ip_address": "147.35.25.192",
-    "last_name": "Dykes"
-  },
-  {
-    "balance": 1471,
-    "email": "sklagge7@dell.com",
-    "first_name": "Sada",
-    "gender": "Female",
-    "img": "https://robohash.org/exercitationemtotamenim.jpg?size=50x50&set=set1",
-    "ip_address": "143.193.248.153",
-    "last_name": "Klagge"
-  },
-  {
-    "balance": 9969.7,
-    "email": "glewerenz8@europa.eu",
-    "first_name": "Genia",
-    "gender": "Female",
-    "img": "https://robohash.org/enimdoloremqueut.jpg?size=50x50&set=set1",
-    "ip_address": "104.0.250.224",
-    "last_name": "Lewerenz"
-  },
-  {
-    "balance": 7253.5,
-    "email": "ddemarchi9@taobao.com",
-    "first_name": "Daloris",
-    "gender": "Female",
-    "img": "https://robohash.org/uteaquearchitecto.jpg?size=50x50&set=set1",
-    "ip_address": "124.166.67.100",
-    "last_name": "De Marchi"
+    "headline": "Tech-stock selloff weighs on Asian markets",
+    "timestamp": "11:21pm Today",
+    "source": "Ese Erheriene"
+  }, {
+    "headline": "China's service-sector activity picks up",
+    "timestamp": "11:01pm Today",
+    "source": "MarketWatch.com"
+  }, {
+    "headline": "Austrailian central bank leaves rates unchanged",
+    "timestamp": "10:51pm Today",
+    "source": "MarketWatch.com"
+  }, {
+    "headline": "Tax cuts are halfway priced into the stock market, J.P. Morgan says",
+    "timestamp": "6:54pm Today",
+    "source": "William Watts"
+  }, {
+    "headline": "Dow finished at record; S&P 500, Nasdaq flounder as tech stocks swoon",
+    "timestamp": "4:54pm Today",
+    "source": "Sue Chang"
+  }, {
+    "headline": "Here's why Venezuela's crytocurrency venture is no panacea",
+    "timestamp": "4:46pm Today",
+    "source": "Anneken Tappe"
   }
 ];
 
